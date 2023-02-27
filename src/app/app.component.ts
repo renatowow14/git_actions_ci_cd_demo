f35f90d60db0799e9ae2b41ec1e641ba87773a9e
renatowow14 <renatogomessilverio@gmail.com>
2023-02-27 12:49:47 -0300

    change name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-github-actions-amazon-s3';
+  title = 'angular-github-actions-amazon-s3-demo';
 }
